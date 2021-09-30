7271762518cdd06d205e602e2f0ce32cc576fa51
AverkinaNastya <Vitanikini14@mail.ru>
2021-09-30 23:22:13 +0300

    Добавьте файлы проекта.

---
+++ LB11-12.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "MyQueue.h"
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, ".1251");
+	queue<double> a(10);
+	a + 1.2;
+	a + 10.2;
+	a + 12.2;
+	a.operator-();
+	a.operator--();
+	a.operator-();
+	a.operator-();
+	a.operator--();
+
+	queue<char> b(3);
+	b + 'a';
+	b + 'w';
+	b.operator-();
+	b.operator--();
+	system("PAUSE");
+}
